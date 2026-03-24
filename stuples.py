ece8779cd431b63ab8f104fd733b8fc5e0ac6d61
ssilviya-sketch <ssilviya@gmail.com>
2026-03-24 17:51:16 +0100

    Add files via upload

---
+++ stuples.py
@@ -0,0 +1,7 @@
+t = (1,2,3,"i","h")
+print(t)
+print(type(t))
+print(t[3])#a tuple is the same thing as a list exept you use round brackets instead of square and u cant add or delete a element to it after it has been created
+for i in t:
+    print(i)
+    #giving the brackets making a tuple is optional
