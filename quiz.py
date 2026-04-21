68e018810c20cdbc8bb27be5f128b14e977f48a8
ssilviya-sketch <ssilviya@gmail.com>
2026-04-21 17:52:01 +0200

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,11 @@
+import pgzrun
+HEIGHT = 700
+WIDTH = 900
+TITLE = "quiz master"
+markbox = Rect(0,0,890,80)
+questionbox = Rect(0,0,700,150)
+timerbox = Rect(0,0,150,150)
+answerbox1 = Rect(0,0,300,150)
+answerbox2 = Rect(0,0,300,150)
+answerbox3 = Rect(0,0,300,150)
+answerbox4 = Rect(0,0,300,150)
